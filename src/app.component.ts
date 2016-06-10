0d934676092a19b91bcbfd466e739a2a24522853
Martin Nuc <martin@nuc.cz>
2016-06-10 16:04:22 +0200

    Fixed lazy loaded components directory name

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, ViewEncapsulation} from "@angular/core";
 import {Routes, ROUTER_DIRECTIVES} from "@angular/router";
-import {Home} from "./components/+home/home.component";
-import {About} from "./components/+about/about.component";
+import {Home} from "./components/home/home.component";
+import {About} from "./components/about/about.component";
 
 @Component({
     selector: 'app',
